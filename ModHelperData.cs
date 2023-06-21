6fdff9e36f5ab2395c1667319f587f21599df97f
LynxCGames <125915289+LynxCGames@users.noreply.github.com>
2023-06-20 23:08:12 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,6 +5,7 @@ public static class ModHelperData
     public const string WorksOnVersion = "37.0";
     public const string Version = "1.0.0";
     public const string Name = "4th Path Overhaul";
+    public const string Author = "LynxC";
 
     public const string Description = "Adds a 4th upgrade path to every tower in the game. 9 towers currently available - " +
         "Dart Monkey, Bomb Shooter, Tack Shooter, Sniper Monkey, Heli Pilot, Wizard, Ninja, Spike Factory, and Engineer." +
@@ -12,6 +13,6 @@ public static class ModHelperData
 
     public const string Dependencies = "doombubbles/paths-plus-plus";
 
-    public const string RepoOwner = "LynxC"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoOwner = "LynxCGames"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Alternate_Paths"; // TODO add your repo name here, also in the download url in README.md
 }
